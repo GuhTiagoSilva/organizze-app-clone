924c453423770e21ce6a3e759bb5d3db7b2e7125
GuhTiagoSilva <gustavo.tiago.gts@outlook.com>
2019-08-15 12:01:52 -0300

    Login and Sign In interfaces configured

--- app/src/main/java/com/example/organizze/MainActivity.java
+++ app/src/main/java/com/example/organizze/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.organizze;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.organizze.activity.LoginActivity;
+import com.example.organizze.activity.SignInActivity;
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+        setButtonNextVisible(false);
+        setButtonBackVisible(false);
+
+        addSlide(new FragmentSlide.Builder().background(android.R.color.white).fragment(R.layout.intro_1).build());
+        addSlide(new FragmentSlide.Builder().background(android.R.color.white).fragment(R.layout.intro_2).build());
+        addSlide(new FragmentSlide.Builder().background(android.R.color.white).fragment(R.layout.intro_3).build());
+        addSlide(new FragmentSlide.Builder().background(android.R.color.white).fragment(R.layout.intro_4).build());
+        addSlide(new FragmentSlide.Builder().background(android.R.color.white).fragment(R.layout.intro_sign_in).build());
+
+
+
+    }
+
+
+    public void enter(View view){
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void createAccount(View view){
+        startActivity(new Intent(this, SignInActivity.class));
     }
 }
